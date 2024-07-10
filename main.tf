858ed9316c2a20ee306ca2456e36d4f212633b61
bhanu <m.bhanuprakash@gmail.com>
2024-07-10 18:23:02 +0530

    Commiting main.tf

---
+++ main.tf
@@ -0,0 +1,204 @@
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_user_assigned_identity" "example" {
+  name                = "example-admin"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+}
+
+# Create a key vault with access policies which allow for the current user to get, list, create, delete, update, recover, purge and getRotationPolicy for the key vault key and also add a key vault access policy for the Microsoft Sql Server instance User Managed Identity to get, wrap, and unwrap key(s)
+resource "azurerm_key_vault" "example" {
+  name                        = "mssqltdeexample"
+  location                    = azurerm_resource_group.example.location
+  resource_group_name         = azurerm_resource_group.example.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = azurerm_user_assigned_identity.example.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = true
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = ["Get", "List", "Create", "Delete", "Update", "Recover", "Purge", "GetRotationPolicy"]
+  }
+
+  access_policy {
+    tenant_id = azurerm_user_assigned_identity.example.tenant_id
+    object_id = azurerm_user_assigned_identity.example.principal_id
+
+    key_permissions = ["Get", "WrapKey", "UnwrapKey"]
+  }
+}
+
+resource "azurerm_key_vault_key" "example" {
+  depends_on = [azurerm_key_vault.example]
+
+  name         = "example-key"
+  key_vault_id = azurerm_key_vault.example.id
+  key_type     = "RSA"
+  key_size     = 2048
+
+  key_opts = ["unwrapKey", "wrapKey"]
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplesa"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_mssql_server" "example" {
+  name                         = "example-sqlserver"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+
+
+resource "azurerm_mssql_database" "example" {
+  name           = "example-db"
+  server_id      = azurerm_mssql_server.example.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 4
+  read_scale     = true
+  sku_name       = "S0"
+  zone_redundant = true
+  enclave_type   = "VBS"
+
+  tags = {
+    foo = "bar"
+  }
+
+   identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.example.id]
+  }
+
+  transparent_data_encryption_key_vault_key_id = azurerm_key_vault_key.example.id
+
+
+  # prevent the possibility of accidental data loss
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "azurerm_mssql_database_extended_auditing_policy" "example" {
+  database_id                             = azurerm_mssql_database.example.id
+  storage_endpoint                        = azurerm_storage_account.example.primary_blob_endpoint
+  storage_account_access_key              = azurerm_storage_account.example.primary_access_key
+  storage_account_access_key_is_secondary = false
+  retention_in_days                       = 6
+}
+
+
+resource "azurerm_mssql_server_security_alert_policy" "example" {
+  resource_group_name = azurerm_resource_group.example.name
+  server_name         = azurerm_sql_server.example.name
+  state               = "Enabled"
+}
+
+resource "azurerm_mssql_server_microsoft_support_auditing_policy" "example" {
+  server_id                  = azurerm_mssql_server.example.id
+  blob_storage_endpoint      = azurerm_storage_account.example.primary_blob_endpoint
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+}
+
+resource "azurerm_mssql_server_vulnerability_assessment" "example" {
+  server_security_alert_policy_id = azurerm_mssql_server_security_alert_policy.example.id
+  storage_container_path          = "${azurerm_storage_account.example.primary_blob_endpoint}${azurerm_storage_container.example.name}/"
+  storage_account_access_key      = azurerm_storage_account.example.primary_access_key
+}
+
+resource "azurerm_mssql_database_vulnerability_assessment_rule_baseline" "example" {
+  server_vulnerability_assessment_id = azurerm_mssql_server_vulnerability_assessment.example.id
+  database_name                      = azurerm_sql_database.example.name
+  rule_id                            = "VA2065"
+  baseline_name                      = "master"
+  baseline_result {
+    result = [
+      "allowedip1",
+      "123.123.123.123",
+      "123.123.123.123"
+    ]
+  }
+  baseline_result {
+    result = [
+      "allowedip2",
+      "255.255.255.255",
+      "255.255.255.255"
+    ]
+  }
+}
+
+# below for sqlserver with failover group
+
+resource "azurerm_mssql_server" "primary" {
+  name                         = "mssqlserver-primary"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "missadministrator"
+  administrator_login_password = "thisIsKat11"
+}
+
+resource "azurerm_mssql_server" "secondary" {
+  name                         = "mssqlserver-secondary"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = "North Europe"
+  version                      = "12.0"
+  administrator_login          = "missadministrator"
+  administrator_login_password = "thisIsKat12"
+}
+
+resource "azurerm_mssql_database" "example" {
+  name        = "exampledb"
+  server_id   = azurerm_mssql_server.primary.id
+  sku_name    = "S1"
+  collation   = "SQL_Latin1_General_CP1_CI_AS"
+  max_size_gb = "200"
+}
+
+resource "azurerm_mssql_failover_group" "example" {
+  name      = "example"
+  server_id = azurerm_mssql_server.primary.id
+  databases = [
+    azurerm_mssql_database.example.id
+  ]
+
+  partner_server {
+    id = azurerm_mssql_server.secondary.id
+  }
+
+  read_write_endpoint_failover_policy {
+    mode          = "Automatic"
+    grace_minutes = 80
+  }
+
+  tags = {
+    environment = "prod"
+    database    = "example"
+  }
+}
+
+
+  resource "azurerm_mssql_firewall_rule" "firewall" {
+    name             = "FirewallRule1"
+  server_id        = azurerm_mssql_server.sqlserver.id
+  start_ip_address = "10.0.17.62"
+  end_ip_address   = "10.0.17.62"
+
+  }
